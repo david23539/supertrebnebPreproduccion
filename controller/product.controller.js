748427789c5ca091ffb3a39164294eb3a242ab3b
david23539 <david23539@gmail.com>
2018-04-12 21:58:33 +0200

    fix on image service

--- controller/product.controller.js
+++ controller/product.controller.js
@@ -149,7 +149,7 @@ function updateProductImage(req, res){
 		if(req.files.image){
 			const filename = serviceProduct.validateImageFile(req.files.image)
 			if(filename){
-				console.log("controller "+ filename);
+
 				serviceProduct.resizeImage(req, constantFile.urls.PRODUCT_IMG_ORIGINAL+filename, constantFile.urls.PRODUCT_IMG_RESIZE+filename)
 				ProductModel.findByIdAndUpdate(productId, {stn_imageProduct:filename, stn_imageProductResize:filename}, {new:true},(err, productUpdate)=>{
 					if(err || !productUpdate){
